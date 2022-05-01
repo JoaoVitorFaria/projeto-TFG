ef89539549a1e4978816d5b63ff7ed7afa6ceec4
Joao Vitor <joaovitorfaria@unifei.edu.br>
2022-05-01 20:18:17 -0300

    Atualizações e comentários

--- lib/controller/requirement_state_controller.dart
+++ lib/controller/requirement_state_controller.dart
@@ -7,24 +7,21 @@ class RequirementStateController extends GetxController {
   final _pauseScanning = false.obs;
 
   bool get bluetoothEnabled => bluetoothState.value == BluetoothState.stateOn;
-  
 
   updateBluetoothState(BluetoothState state) {
     bluetoothState.value = state;
   }
-
-  
-
+  // Atualiza o estado do scanner 
   startScanning() {
     _startScanning.value = true;
     _pauseScanning.value = false;
   }
-
+  // Atualiza o estado do scanner 
   pauseScanning() {
     _startScanning.value = false;
     _pauseScanning.value = true;
   }
-
+  // uma variavel assincrona 
   Stream<bool> get startStream {
     return _startScanning.stream;
   }
